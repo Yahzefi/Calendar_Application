1f1d4f6c8f797912965bcc633bc15e3b322c558c
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-03-09 01:04:09 -0500

    Add files via upload

---
+++ src/components/Day.js
@@ -0,0 +1,10 @@
+
+const Day = () => {
+    return (
+        <div style={{border: "thin solid green"}}>
+            <h2>Day</h2>
+        </div>
+    )
+}
+
+export default Day
---
+++ src/components/Reminders.js
@@ -0,0 +1,10 @@
+
+const Reminders = () => {
+    return (
+        <div className="r-cont" style={{border: "thin solid red"}}>
+            <h2>Reminders</h2>
+        </div>
+    )
+}
+
+export default Reminders
