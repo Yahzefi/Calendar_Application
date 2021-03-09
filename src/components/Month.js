d50027e060ef0da1d922cf79a1fbc7e43ff7a0c5
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-03-09 01:05:24 -0500

    Update Month.js

--- src/components/Month.js
+++ src/components/Month.js
@@ -1,1 +1,16 @@
-// 
+import Days from "./Days"
+
+const Month = () => {
+    return (
+        <div className="m-cont" style={{border: "thin solid white"}}>
+            {/* h2 below will be deleted later on */}
+            <h2 style={{gridRow:"1/1"}}>Month</h2>
+            <div className="img-cont">
+                <h4>Image Goes Here</h4>
+            </div>
+            <Days />
+        </div>
+    )
+}
+
+export default Month
