680419b80439716f727ba7a8ab52387851614127
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-03-09 01:04:44 -0500

    Update Days.js

--- src/components/Days.js
+++ src/components/Days.js
@@ -1,1 +1,12 @@
-// 
+import Day from "./Day"
+
+const Days = () => {
+    return (
+        <div className="days-cont" style={{border: "thin solid skyblue"}}>
+            <h2>Days</h2>
+            <Day />
+        </div>
+    )
+}
+
+export default Days
