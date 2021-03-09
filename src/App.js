b4f2fb8b58d5f481462317fe30004edfc8f10986
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-03-09 01:05:42 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,33 @@ import './App.css';
 import Calendar from "./components/Calendar";
 
 function App() {
-  const [showCal, setCalDisplay] = useState(true)
-  const [x, setX] = useState(false)
-  // let changeBackground = ()=>{
-  //   setX(!x);
-  // }
+
+  const [showCal, setCalDisplay] = useState(false)
+
+
   return (
-    <div className="primary_container" onClick={()=>setCalDisplay(!showCal)}>
-      {showCal && <Calendar color={x ? "green" : "yellow"} onToggle={()=>setX(!x)}/>}
+    <div className="primary_container">
+
+      {!showCal && 
+        <button 
+          id="showCal_btn" 
+          className="btn btn-success"
+          onClick={()=>setCalDisplay(!showCal)}
+        >Show Calendar
+        </button>
+      }
+
+      {showCal && <Calendar />}
+
+      {showCal && 
+        <button 
+          id="hideCal_btn" 
+          className="btn btn-danger"
+          onClick={()=>setCalDisplay(!showCal)}
+        >Hide Calendar
+        </button>
+      }
+
     </div>
   );
 }
