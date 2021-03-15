387be334295fa925f6056e3ec4f5356849405e54
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-03-15 01:43:38 -0400

    Update Calendar.js

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,30 +1,15 @@
-// import PropTypes from 'prop-types';
 import Month from "./Month";
 import Reminders from "./Reminders";
-// Month Image Imports
-import jan_img from '../Images/January_Background.jpg'
 
-const Calendar = ({ currentMonth }) => {
-    // const backgroundImage = () => {
-    //     switch(currentMonth){
-    //         case "January":
-    //             return jan_img
-    //         default:
-    //             throw new Error("Month Not Found")
-    //     }
-    // }
+import { useState } from "react"
 
+const Calendar = () => {
     return (
         <div id="cal_container" className="calendar_container">
-            <Month month={currentMonth} month_img={jan_img}/>
+            <Month />
             <Reminders />
         </div>
     )
 }
-
-// Calendar.defaultProps = {
-//     color: "lightgrey"
-// }
-
 
 export default Calendar
