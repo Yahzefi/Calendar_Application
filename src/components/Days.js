361c6f23557f4df33f4ec16f1b661847e5ac61cc
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-03-18 02:06:57 -0400

    Update Days.js

--- src/components/Days.js
+++ src/components/Days.js
@@ -1,14 +1,19 @@
 import Day from "./Day"
 
+import { useLocation } from "react-router-dom"
+
 const Days = () => {
+    const location = useLocation()
     let daysArr = [];
     for(let i=1; i < 36; i++){
         daysArr.push(i)
     }
     return (
         <div className="days-cont">
-            {daysArr.map((day)=>(
+            {location.pathname === "/january" ? daysArr.map((day)=>(
                 <Day key={"Day " + day} dayNumber={day < 32 ? day : ""}/>
+            )): daysArr.map((day)=>(
+                <Day key={"Day " + day} dayNumber={day < 5 ? "" : day < 36 ? (day - 4): ""}/>
             ))}
         </div>
     )
