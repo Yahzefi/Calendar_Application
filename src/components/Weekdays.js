fd01fceb93731b2be9b396530d48614e6dfdcd89
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-03-21 03:01:43 -0400

    Add files via upload

---
+++ src/components/Weekdays.js
@@ -0,0 +1,16 @@
+
+const Weekdays = () => {
+    return (
+        <div className="wDays-cont">
+            <h4>Sun</h4>
+            <h4>Mon</h4>
+            <h4>Tue</h4>
+            <h4>Wed</h4>
+            <h4>Thu</h4>
+            <h4>Fri</h4>
+            <h4>Sat</h4>
+        </div>
+    )
+}
+
+export default Weekdays
