eea9730c315d01866d4a2bbbeb7cdb30b0510212
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-03-28 00:19:23 -0400

    Update Reminders.js

--- src/components/Reminders.js
+++ src/components/Reminders.js
@@ -1,8 +1,14 @@
+import { useState } from "react"
 
-const Reminders = () => {
+
+const Reminders = ({ addReminder }) => {
+    const [text, setText] = useState("")
     return (
         <div className="r-cont" style={{borderLeft: "2px dashed black"}}>
             <h2>Reminders</h2>
+            <input type="text" className="remind-inp" placeholder="Type a Reminder" value={text} onChange={(e)=>setText(e.target.value)}/>
+            <button id="remind_btn" className="sm-btn btn-info" onClick={()=>{addReminder(text);setText("")}}>Add Reminder</button>
+            <ol id="r_list"></ol>
         </div>
     )
 }
