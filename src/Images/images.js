0c3e6ed3aef9337131f000d466229affb5cab2b5
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-03-28 00:17:48 -0400

    Update images.js

--- src/Images/images.js
+++ src/Images/images.js
@@ -1,5 +1,27 @@
 import jan_img from "./month-backgrounds/January_Background.jpg"
+import feb_img from "./month-backgrounds/February_Background.jpg"
+import mar_img from "./month-backgrounds/March_Background.jpg"
+import apr_img from "./month-backgrounds/April_Background.jpg"
+import may_img from "./month-backgrounds/May_Background.jpg"
+import jun_img from "./month-backgrounds/June_Background.jpg"
+import jul_img from "./month-backgrounds/July_Background.jpg"
+import aug_img from "./month-backgrounds/August_Background.jpg"
+import sep_img from "./month-backgrounds/September_Background.jpg"
+import oct_img from "./month-backgrounds/October_Background.jpg"
+import nov_img from "./month-backgrounds/November_Background.jpg"
+import dec_img from "./month-backgrounds/December_Background.jpg"
 
 export const MONTH_BACKGROUNDS = {
-    January: jan_img
+    January: jan_img,
+    February: feb_img,
+    March: mar_img,
+    April: apr_img,
+    May: may_img,
+    June: jun_img,
+    July: jul_img,
+    August: aug_img,
+    September: sep_img,
+    October: oct_img,
+    November: nov_img,
+    December: dec_img
 }
