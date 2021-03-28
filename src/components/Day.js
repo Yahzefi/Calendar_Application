9ae85924246692dfb5ef317dc7241cb5ab640c05
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-03-28 00:18:51 -0400

    Update Day.js

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,7 +1,19 @@
+import { useState, useEffect } from "react"
+import { useLocation } from "react-router-dom"
 
-const Day = ({ dayNumber, dayClick }) => {
+const Day = ({ dayNumber }) => {
+    const location = useLocation()
+    const [background, setBackground] = useState("")
+    useEffect(()=>{
+        setBackground("") 
+    } ,[location])
     return (
-        <div id={"Day " + dayNumber} className="day-cont" style={{border: "thin solid black"}} onClick={dayClick}>
+        <div 
+        id={
+            "Day " + dayNumber
+            // WHERE TO PUT CONTEXT 
+        } 
+        className="day-cont" style={{border: "thin solid black", backgroundColor: background}} onClick={()=>background === "" ? setBackground("red") : setBackground("")}>
             <p>{dayNumber}</p>
         </div>
     )
