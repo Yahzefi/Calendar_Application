2f9ab4f753e014e21436799ab49ccde75fdfc93a
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-03-28 00:18:59 -0400

    Update Days.js

--- src/components/Days.js
+++ src/components/Days.js
@@ -2,7 +2,7 @@ import Day from "./Day"
 
 import { useLocation } from "react-router-dom"
 
-const Days = ({ dayClick }) => {
+const Days = () => {
     const location = useLocation()
     let daysArr = [];
     for(let i=1; i < 43; i++){
@@ -11,29 +11,29 @@ const Days = ({ dayClick }) => {
     return (
         <div className="days-cont">
             {location.pathname === "/january" ? daysArr.map((day)=>(
-                <Day key={"Day " + day} dayNumber={day < 6 ? "" : day < 37 ? (day-5): ""} dayClick={dayClick}/>
+                <Day key={"Day " + day} dayNumber={day < 6 ? "" : day < 37 ? (day-5): ""} />
             )): location.pathname === "/february" ? daysArr.map((day)=>(
-                <Day key={"Day " + day} dayNumber={day < 2 ? "" : day < 30 ? (day - 1): ""} dayClick={dayClick}/>
+                <Day key={"Day " + day} dayNumber={day < 2 ? "" : day < 30 ? (day - 1): ""} />
             )): location.pathname === "/march" ? daysArr.map((day)=>(
-                <Day key={"Day " + day} dayNumber={day < 2 ? "" : day < 33 ? (day - 1): ""} dayClick={dayClick}/>
+                <Day key={"Day " + day} dayNumber={day < 2 ? "" : day < 33 ? (day - 1): ""} />
             )): location.pathname === "/april" ? daysArr.map((day)=>(
-                <Day key={"Day " + day} dayNumber={day < 5 ? "" : day < 35 ? (day - 4): ""} dayClick={dayClick}/>
+                <Day key={"Day " + day} dayNumber={day < 5 ? "" : day < 35 ? (day - 4): ""} />
             )): location.pathname === "/may" ? daysArr.map((day)=>(
-                <Day key={"Day " + day} dayNumber={day < 7 ? "" : day < 38 ? (day - 6): ""} dayClick={dayClick}/>
+                <Day key={"Day " + day} dayNumber={day < 7 ? "" : day < 38 ? (day - 6): ""} />
             )): location.pathname === '/june' ? daysArr.map((day)=>(
-                <Day key={"Day " + day} dayNumber={day < 3 ? "" : day < 33 ? (day - 2): ""} dayClick={dayClick}/>
+                <Day key={"Day " + day} dayNumber={day < 3 ? "" : day < 33 ? (day - 2): ""} />
             )): location.pathname === "/july" ? daysArr.map((day)=>(
-                <Day key={"Day " + day} dayNumber={day < 5 ? "" : day < 36 ? (day - 4): ""} dayClick={dayClick}/>
+                <Day key={"Day " + day} dayNumber={day < 5 ? "" : day < 36 ? (day - 4): ""} />
             )): location.pathname === "/august" ? daysArr.map((day)=>(
-                <Day key={"Day " + day} dayNumber={day < 32 ? day : ""} dayClick={dayClick}/>
+                <Day key={"Day " + day} dayNumber={day < 32 ? day : ""} />
             )): location.pathname === "/september" ? daysArr.map((day)=>(
-                <Day key={"Day " + day} dayNumber={day < 4 ? "" : day < 34 ? (day - 3): ""} dayClick={dayClick}/>
+                <Day key={"Day " + day} dayNumber={day < 4 ? "" : day < 34 ? (day - 3): ""} />
             )): location.pathname === "/october" ? daysArr.map((day)=>(
-                <Day key={"Day " + day} dayNumber={day < 6 ? "" : day < 37 ? (day - 5) : ""} dayClick={dayClick}/>
+                <Day key={"Day " + day} dayNumber={day < 6 ? "" : day < 37 ? (day - 5) : ""} />
             )): location.pathname === "/november" ? daysArr.map((day)=>(
-                <Day key={"Day " + day} dayNumber={day < 2 ? "" : day < 32 ? (day - 1): ""} dayClick={dayClick}/>
+                <Day key={"Day " + day} dayNumber={day < 2 ? "" : day < 32 ? (day - 1): ""} />
             )): location.pathname === "/december" ? daysArr.map((day)=>(
-                <Day key={"Day " + day} dayNumber={day < 4 ? "" : day < 35 ? (day - 3): ""} dayClick={dayClick}/>
+                <Day key={"Day " + day} dayNumber={day < 4 ? "" : day < 35 ? (day - 3): ""} />
             )): ""}
         </div>
     )
