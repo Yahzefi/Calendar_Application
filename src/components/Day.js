349495ff7f0e82dc5a9ad03d40a76953bc0b82f1
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-04-23 02:06:50 -0400

    Update Day.js

--- src/components/Day.js
+++ src/components/Day.js
@@ -4,15 +4,13 @@ import { useLocation } from "react-router-dom"
 const Day = ({ dayNumber }) => {
     const location = useLocation()
     const [background, setBackground] = useState("")
+    // Background constant acts as a "over" or "not over" marker for each day on the calendar
     useEffect(()=>{
         setBackground("") 
     } ,[location])
     return (
         <div 
-        id={
-            "Day " + dayNumber
-            // WHERE TO PUT CONTEXT 
-        } 
+        id={"Day " + dayNumber} 
         className="day-cont" style={{border: "thin solid black", backgroundColor: background}} onClick={()=>background === "" ? setBackground("red") : setBackground("")}>
             <p>{dayNumber}</p>
         </div>
